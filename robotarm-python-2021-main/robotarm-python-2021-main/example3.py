f8d0bb6d03eb14efebc4b461813f3811fcb5968a
Lukas <Lukas.v.hulst@gmail.com>
2021-09-20 12:10:22 +0200

    exercise3 gemaakt

---
+++ robotarm-python-2021-main/robotarm-python-2021-main/example3.py
@@ -0,0 +1,14 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 3')
+robotArm.speed = 3
+# Jouw python instructies zet je vanaf hier:
+
+for x in range(0, 4):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+
+# Na jouw code wachten tot het sluiten van de window:
+robotArm.wait()
